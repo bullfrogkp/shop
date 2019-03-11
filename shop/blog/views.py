6f00d33907daf4fb5ca95eff91e8963c989e02bd
pqhitpan <pqhitpan@gmail.com>
2019-03-11 03:55:01 +0000

    iii

--- shop/blog/views.py
+++ shop/blog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from .models import Post
+import markdown
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Category
 
 def index(request):
     post_list = Post.objects.all().order_by('-created_time')
@@ -7,4 +8,23 @@ def index(request):
 
 def detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
+    post.increase_views()
+    post.body = markdown.markdown(post.body,
+                                  extensions=[
+                                     'markdown.extensions.extra',
+                                     'markdown.extensions.codehilite',
+                                     'markdown.extensions.toc',
+                                  ])
     return render(request, 'blog/detail.html', context={'post': post})
+
+def archives(request, year, month):
+    post_list = Post.objects.filter(created_time__year=year,
+                                    created_time__month=month
+                                    ).order_by('-created_time')
+    return render(request, 'blog/index.html', context={'post_list': post_list})
+
+def category(request, pk):
+    # 记得在开始部分导入 Category 类
+    cate = get_object_or_404(Category, pk=pk)
+    post_list = Post.objects.filter(category=cate).order_by('-created_time')
+    return render(request, 'blog/index.html', context={'post_list': post_list})
